683a9fc31f97db16a565774eac19d7fbfead5bfc
devruchoa <ronaldo.uchoa.pessoal@gmail.com>
2025-02-05 17:23:06 -0300

    chore: start do projeto

---
+++ src/test/java/com/academico/sistemaacademico/SistemaAcademicoSimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academico.sistemaacademico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAcademicoSimplificadoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
